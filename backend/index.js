879a8e58c87408d9e1d261e2a62e25cf80fb07d3
Karthik konduru <konduru21bcs93@iiitkotyayam.ac.in>
2024-06-24 10:43:11 +0530

    7th update

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(cors());
 mongoDB()
 
 app.use((req,res,next) =>{
-    
+    res.send("server is running")
     res.setHeader("Access-Control-Allow-Origin","http://localhost:3000")
     res.header(
         "Access-Control-Allow-Headers",
