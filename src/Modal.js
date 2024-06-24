f3209d164406ccf2306ba694d4e645bf04bc7b30
Karthik konduru <konduru21bcs93@iiitkotyayam.ac.in>
2024-06-24 22:10:29 +0530

    new update

--- src/Modal.js
+++ src/Modal.js
@@ -2,33 +2,13 @@
 import React from "react";
 import ReactDom from "react-dom"
 
-const MODAL_STYLES = {
-  position: "fixed",
-  top: "50%",
-  left: "50%",
-  backgroundColor: "rgb(0,0,0)",
-  transform: "translate(-50%,-50%)",
-  zIndex: 1000,
-  height: "90%",
-  width: "90%",
-};
 
-const OVERLAY_STYLES = {
-  postion: "fixed",
-  top: 0,
-  left: 0,
-  right: 0,
-  bottom: 0,
-  backgroundColor: "rgba(0,0,0,0.7)",
-  zIndex: 1000
-  
-}; 
 function Modal({ children, onClose }) {
   return ReactDom.createPortal(
     <>
-      <div className="modal-backdrop" >
+      <div className="modal-backdrop " >
       {/* style={OVERLAY_STYLES} */}
-        <div className="modal-content">
+        <div className="modal-content bg-foodDetails dark:bg-darkCard">
         {/* style={MODAL_STYLES} */}
           <button
             type="button"
